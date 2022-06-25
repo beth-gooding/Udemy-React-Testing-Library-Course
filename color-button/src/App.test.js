92c96dfb495b5b96ff19ad766b08dae084da0191
Beth Gooding <beth.gooding@hotmail.com>
2022-06-25 12:51:46 +0100

    Updates test from using regex to literal string

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 test('renders learn react link', () => {
   render(<App />); // Creates a virtual DOM for whatever jsx you give it
   // Access the virtual DOM using the screen method
-  const linkElement = screen.getByText(/learn react/i); // get an element by text
+  const linkElement = screen.getByText('Learn React'); // get an element by text
   expect(linkElement).toBeInTheDocument(); // this is an assertion 
 });

7ffcd1334409a7e4a33fc32b7f652865e372d65f
Beth Gooding <beth.gooding@hotmail.com>
2022-08-31 16:13:08 +0100

    Test cases for a colour button

--- color-button/notes.md
+++ color-button/notes.md
@@ -4,3 +4,6 @@
 - Can look online to see a list of all the roles available to you
 - If you have multiple elements with the same role, you can use name to find the text on that specific element
 - If you can't find an element in the way a screenreader does, then it won't be accessible for a user
+
+- Always you have the option to give a role that does not exist
+- This will give an error from RTL, but it will tell you all the roles that are available in your component
--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,9 +1,22 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />); // Creates a virtual DOM for whatever jsx you give it
-  // Access the virtual DOM using the screen method
-  const linkElement = screen.getByRole('link', {name: /learn react/i}); // get an element by role, using it's name i.e. text
-  expect(linkElement).toBeInTheDocument(); // this is an assertion 
+test('button has the correct initial colour', () => {
+  render(<App />);
+
+  // find an element with a role of button and a text of 'Change to blue'
+  const colorButton = screen.getByRole('button', { name: 'Change to blue'}); // name = display text
+
+  // use toHaveStyle to check the styles of the elements, expect red background
+  expect(colorButton).toHaveStyle({ backgroundColor: 'red' });
+
+  // click button
+  fireEvent.click(colorButton);
+
+  // expect the background colour to be blue
+  expect(colorButton).toHaveStyle({ backgroundColor: 'blue'});
+
+  // expect the button text to be 'Change to red'
+  expect(colorButton.textContent).toBe('Change to red');
 });
+
